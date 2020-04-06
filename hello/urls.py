4769fd745f25d8fcc897e03507d6b78bbf37c971
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2020-04-06 22:57:22 +0530

    Migrated admin data and altered the admin titles

--- hello/urls.py
+++ hello/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Icecream Admin'
+admin.site.site_title = 'Icecream admin portal'
+admin.site.index_title = 'Welcome to Icecreams site'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
