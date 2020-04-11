30d1677619c301743d91b200115477840dc01918
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2020-04-11 23:02:35 +0530

    Chnaged models.py to show the submitter name in the contact object in the admin panel

--- home/models.py
+++ home/models.py
@@ -8,3 +8,6 @@ class Contact(models.Model) :
     needs = models.CharField(max_length=50)
     message = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
