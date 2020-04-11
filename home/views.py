ef5ff43a58e239354374455d6365b04c6e35e389
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2020-04-11 19:04:51 +0530

    Getting homepage ready

--- home/views.py
+++ home/views.py
@@ -13,12 +13,12 @@ def index(request):
 
 # Creating index function
 def about(request):
-    return HttpResponse('This is about page')
+    return render(request, 'about.html')
 
 # Creating index function
 def contact(request):
-    return HttpResponse('This is contact page')
+    return render(request, 'contact.html')
 
 # Creating index function
 def services(request):
-    return HttpResponse('This is services page')    
+    return render(request, 'services.html')
