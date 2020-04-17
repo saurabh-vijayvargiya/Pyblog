fc63e941ba21ffedd57ada9746d4ebc4d74b133c
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2020-04-17 21:49:40 +0530

    Resolved merge conflict

--- home/models.py
+++ home/models.py
@@ -8,3 +8,25 @@ class Contact(models.Model) :
     needs = models.CharField(max_length=50)
     message = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.firstname + ' ' + self.lastname
+
+# Create Author model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
+
+# Create category model
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+
+# Create Article model
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/')
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
